39bc6f66d1985db34bdbd27a8f97a2237d17171d
yangli <yli@stellarcyber.ai>
2021-01-01 13:13:49 +0800

    新增Zuul网关，并以自定义的随机负载均衡策略路由请求到consumer

--- cloud-consumer-2/src/main/java/pri/liyang/consumer/fallback/CloudConsumerServiceFallback.java
+++ cloud-consumer-2/src/main/java/pri/liyang/consumer/fallback/CloudConsumerServiceFallback.java
@@ -1,6 +1,7 @@
 package pri.liyang.consumer.fallback;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pri.liyang.consumer.service.CloudConsumerService;
@@ -10,41 +11,44 @@ import pri.liyang.consumer.service.CloudConsumerService;
 @RequestMapping("/hystrix")
 public class CloudConsumerServiceFallback implements CloudConsumerService {
 
+    @Value("${server.port}")
+    private Integer port;
+
     @Override
     public String init() {
-        return "init()方法调用出错，这是服务降级的处理方法";
+        return "init()方法调用出错，这是服务降级的处理方法【consumer端口：" + port + "】";
     }
 
     @Override
     public String getTime() {
-        return "getTime()方法调用出错，这是服务降级的处理方法";
+        return "getTime()方法调用出错，这是服务降级的处理方法【consumer端口：" + port + "】";
     }
 
     @Override
     public String register(String name, int age) {
         String params = "name = " + name + ", age = " + age;
-        return "getTime()方法调用出错，参数：" + params + "，这是服务降级的处理方法";
+        return "getTime()方法调用出错，参数：" + params + "，这是服务降级的处理方法【consumer端口：" + port + "】";
     }
 
     @Override
     public String greeting(String name, int age) {
         String params = "name = " + name + ", age = " + age;
-        return "greeting()方法调用出错，参数：" + params + "，这是服务降级的处理方法";
+        return "greeting()方法调用出错，参数：" + params + "，这是服务降级的处理方法【consumer端口：" + port + "】";
     }
 
     @Override
     public String halfError() {
-        return "你碰到那一半的出错概率了，这是服务降级的处理方法";
+        return "你碰到那一半的出错概率了，这是服务降级的处理方法【consumer端口：" + port + "】";
     }
 
     @Override
     public String quarterError() {
-        return "你碰到那四分之一的出错概率了，这是服务降级的处理方法";
+        return "你碰到那四分之一的出错概率了，这是服务降级的处理方法【consumer端口：" + port + "】";
     }
 
     @Override
     public String customError(Double errorProbability) {
-        return "你碰到那" + (errorProbability * 100) + "%的出错概率了，这是服务降级的处理方法";
+        return "你碰到那" + (errorProbability * 100) + "%的出错概率了，这是服务降级的处理方法【consumer端口：" + port + "】";
     }
 
 }
